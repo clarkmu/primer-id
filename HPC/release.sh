3624f8880f08ab62d5f5db8c4b566c58bbb1d2af
Michael <clarkmu@gmail.com>
2024-08-25 10:53:27 -0400

    unignore src/bin/intactness

--- HPC/release.sh
+++ HPC/release.sh
@@ -4,5 +4,5 @@
 
 #apt install musl-tools
 #rustup target list
-#rustup target add x86_64-unknown-linux-musl
-#cargo build --release --target=x86_64-unknown-linux-musl
+#rustup target add x86_64-unknown-linux-gnu
+#cargo build --release --target=x86_64-unknown-linux-gnu
